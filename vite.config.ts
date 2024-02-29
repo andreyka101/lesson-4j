272f25ffcb08689c226ffb1f8f280317938c4af0
andreyka101 <andrejkvasnin236@gmail.com>
2024-02-29 22:03:05 +0300

    efefef

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-export default {
-  base: '/lesson-4j/'
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/lesson-4j/',
   plugins: [
     vue(),
     vueJsx(),
