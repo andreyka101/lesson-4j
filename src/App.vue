e9356fa0d5d97277a47adfd3362adea5755873a4
andreyka101 <andrejkvasnin236@gmail.com>
2024-03-01 17:36:33 +0300

    kikkiki

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
+import {ref} from 'vue'
+import bip from './components/simpleExample/Example3Vi1.Vue'
 let str = "hi"
-let num = 0
+let num = ref(0)
 </script>
 
 <template>
@@ -27,14 +29,45 @@ let num = 0
         <span class="title">
             пример 2
         </span>
-        <div>
-            <button @click="num++" class="GLASSlOLLIPOPS_button">
-                {{num}}
-            </button>
-            <button @click="num++" class="GLASSlOLLIPOPS_button">
-                {{num}}
-            </button>
-        </div>
+        <div style="display: flex;">
+            <div style="display: flex;flex-direction: column;align-items: center;">
+                <span>
+                    +2
+                </span>
+                <button @click="num+=2" class="GLASSlOLLIPOPS_button">
+                    {{num}}
+                </button>
+            </div>
+            <div style="display: flex;flex-direction: column;align-items: center;">
+                <span>
+                    +1
+                </span>
+                <button @click="num++" class="GLASSlOLLIPOPS_button">
+                    {{num}}
+                </button>
+            </div>
+            </div>
+    </span>
+    <span class="GLASSlOLLIPOPS_span spanFlex">
+        <span class="title">
+            пример 2
+        </span>
+        <div style="display: flex;">
+            <div style="display: flex;flex-direction: column;align-items: center;">
+                <span>
+                    +2
+                </span>
+                <bip class="GLASSlOLLIPOPS_button">
+                </bip>
+            </div>
+            <div style="display: flex;flex-direction: column;align-items: center;">
+                <span>
+                    +1
+                </span>
+                <bip class="GLASSlOLLIPOPS_button">
+                </bip>
+            </div>
+            </div>
     </span>
 </div>
 </template>
