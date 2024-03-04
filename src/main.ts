dae6f68e139aef181f84da169ab2ab7651eafe34
andreyka101 <andrejkvasnin236@gmail.com>
2024-03-04 20:29:25 +0300

    efefef

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
 let liveBackground = new LiveBackgroundMod()
 liveBackground.Color.deleteALL()
 liveBackground.Color.append(["292929"])
