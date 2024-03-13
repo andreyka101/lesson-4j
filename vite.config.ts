5a6ef17aa133d06ed60ad69a52a38a902a91d219
Andreka101 <andrejkvasnin236@gmail.com>
2024-03-13 21:40:09 +0300

    3к3кк3

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
