ea4249828e39163032785d5071deb57135b08163
andreyka101 <andrejkvasnin236@gmail.com>
2024-11-15 10:52:36 +0300

    effff

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 <template>
-<!-- всё что мы пишем в App.vue останется на всех страницах-->
-<!-- <div class="GLASSlOLLIPOPS_div" style="text-align: center;">
-    от меня не спрячешься 
-</div> -->
-
-<!-- все страницы отображаются в RouterView -->
-<RouterView></RouterView>
-
+    <!-- все страницы отображаются в RouterView -->
+    <RouterView></RouterView>
+    
+    <!-- всё что мы пишем в App.vue останется на всех страницах-->
+    <div class="GLASSlOLLIPOPS_div" style="text-align: center;">
+        от меня не спрячешься 
+    </div>
 </template>
 
 <style scoped lang="scss">
