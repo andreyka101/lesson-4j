f76a289c02d7efc7412b8961ba1bbb4a569de246
andreyka101 <andrejkvasnin236@gmail.com>
2025-01-17 22:40:23 +0300

    efefefef

--- src/components/simpleExample/Example7Vi1.vue
+++ src/components/simpleExample/Example7Vi1.vue
@@ -3,12 +3,15 @@ import { log } from 'console';
 import { ref } from 'vue'
 const { arr } = defineProps(['arr'])
 let num = ref(0)
-let arr_loc = ref(arr)
+let arr_loc = [] as Array<number>
 function arr_click(){
     num.value++
     for(let i in arr){
-        arr_loc.value[i] += num
+        // arr_loc.push(arr[i])
+        arr[i] += num.value
     }
+    console.log(arr)
+    console.log(arr_loc)
 }
 
 </script>
@@ -16,7 +19,7 @@ function arr_click(){
     <div style="display: flex;flex-direction: column;align-items: center; margin: 0 10px;">
         arr = {{ arr }}
         <button @click="arr_click()"  class="GLASSlOLLIPOPS_button">
-            {{ arr_loc }}
+            {{ arr }}
         </button>
     </div>
 </template>
