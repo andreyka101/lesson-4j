15c4d62f0e565f17e3610cee4c645ec30e13394e
andreyka101 <andrejkvasnin236@gmail.com>
2025-03-22 15:26:31 +0300

    wddwwd

--- src/components/simpleExample/Example19Vi1.vue
+++ src/components/simpleExample/Example19Vi1.vue
@@ -15,40 +15,47 @@ export default {
     },
     //SECTION -  Это все Хуки жизненного цикла :
 
-    //LINK - beforeCreate - срабатывает после того как Рендерер сталкивается с компонентом
+    //LINK - beforeCreate - срабатывает после того как Рендерер сталкивается с этим компонентом
+    // (инициализации Options API не произошла: переменные и функции использовать нельзя)
     beforeCreate() {
         console.log("beforeCreate  /", Date.now())
+        console.log("beforeCreate - num =" , this.num)
     },
-    //LINK - create - срабатывает после Настройки API параметров инициализации
+    // https://v3.ru.vuejs.org/ru/api/options-api.html
+    //LINK - create - срабатывает после инициализации Options API
+    // (переменные и функции использовать можно)
     created() {
         console.log("create  /", Date.now())
+        console.log("created - num =" , this.num)
     },
-    //LINK - beforeMount - срабатывает после Компиляции шаблонов компонента (если шаблоны есть)
+    //LINK - beforeMount - срабатывает перед монтированием (создания и вставки) компонента в DOM
     beforeMount() {
         console.log("beforeMount  /", Date.now())
     },
-    //LINK - mounted - срабатывает после Начального рендеринга создания и вставки DOM узлов
+    //LINK - mounted - срабатывает после монтирования (создания и вставки) компонента в DOM
     mounted() {
         console.log("mounted  /", Date.now())
     },
-    //LINK - beforeUpdate - срабатывает после Изменения данных
+    //LINK - beforeUpdate - срабатывает перед обновлением компонента , но после изменения данных
     beforeUpdate() {
         console.log("beforeUpdate  /", Date.now())
         console.log("beforeUpdate - num =" , this.num)
     },
-    //LINK - updated - срабатывает после Повторного рендера этого компонента уже с именёными данными
+    //LINK - updated - срабатывает после обновления компонента (после повторного рендера этого компонента с внесёнными данными)
     updated() {
         console.log("updated  /", Date.now())
         console.log("updated - num =" , this.num)
     },
     // https://www.book2s.com/tutorials/vuejs-options-beforeunmount.html
-
-    //FIXME - Когда компонент размонтируется
-    //FIXME - Размонти ровано
-    //LINK - beforeUnmount - вызывается перед размонтированием (удалением из DOM)
+    //LINK - beforeUnmount - вызывается перед размонтированием (перед удалением из DOM) всего компонента , а не элемента (кнопка)
+    // По этому если сработает v-if кнопки этого компонента то beforeUnmount не будет вызван (мы удаляем из DOM кнопку ,а не компонент)
+    // Но beforeUnmount сработает при переходе через router или при переходе на другую страницу
     beforeUnmount() {
         console.log("beforeUnmount  /", Date.now())
     },
+    //LINK - unmounted - вызывается после размонтирования (после удаления из DOM) всего компонента , а не элемента (кнопка)
+    // По этому если сработает v-if кнопки этого компонента то unmounted не будет вызван (мы удаляем из DOM кнопку ,а не компонент)
+    // Но unmounted сработает при переходе через router или при переходе на другую страницу
     unmounted() {
         console.log("unmounted  /", Date.now())
     },
